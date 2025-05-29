6b3685e36895fd471b8854c29f686d83fe42c94f
ssp <qkrtjdtlr02@naver.com>
2025-05-29 11:06:07 +0900

    이미지 깨지는 부분 변경

---
+++ app/testImage/page.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable @next/next/no-img-element */
+import { useQuery } from "@tanstack/react-query";
+
+interface PicsumImage {
+  id: string;
+  author: string;
+  width: number;
+  height: number;
+  url: string;
+  download_url: string;
+}
+
+const fetchPicsumImages = async (): Promise<PicsumImage[]> => {
+  const response = await fetch("https://picsum.photos/v2/list?limit=30");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
+
+const TestImagePage = () => {
+  const { data } = useQuery({
+    queryKey: ["picsumImages"],
+    queryFn: fetchPicsumImages,
+  });
+
+  return (
+    <section>
+      <h3 className="flex justify-center font-bold text-2xl mt-4">
+        테스트 이미지
+      </h3>
+
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-5 p-5">
+        {data?.map((image) => (
+          <div key={image.id} className="border border-gray-300 rounded-md p-2">
+            <div className="relative w-[300px] h-[300px]">
+              <img
+                src={`https://picsum.photos/id/${image.id}/300/300`}
+                alt={`Photo by ${image.author}`}
+                width={300}
+                height={300}
+              />
+            </div>
+            <div className="p-2.5 bg-[#f8f8f8]">
+              <p className="text-sm text-gray-500 my-[5px]">
+                Author: {image.author}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default TestImagePage;
