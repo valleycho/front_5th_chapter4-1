1deba971cfcbf0f5e07a20f1c25d6d66129ce9c7
ssp <qkrtjdtlr02@naver.com>
2025-05-29 10:47:26 +0900

    이미지 최적화 비활성화 해제

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    unoptimized: true,
     domains: ['picsum.photos'],
   },
   output: "export",
